06228f7ed7882c5539a7777c227c97ff3ba7489f
electronickai <electronickai@gmail.com>
2020-09-25 10:43:25 +0200

    First beginnings

---
+++ src/main/java/core/Core.java
@@ -0,0 +1,37 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Core {
+
+    List<String> talks = new ArrayList<>();
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public void doNothing() {
+    }
+
+    public void doUnneccessaryStuff() {
+        String s;
+        int result = 41;
+    }
+
+    public List<String> addNewTalk(List<String> list, String talk) {
+        List<String> mutableList = new ArrayList<>(list);
+        if (talk != null) {
+            mutableList.add(talk);
+        }
+        return Collections.unmodifiableList(mutableList);
+    }
+
+ //    public List<String> addBoeseNewTalk(List<String> list, String talk) {
+ //       talks = list;
+ //       return Collections.unmodifiableList(talks);
+ //   }
+
+}
